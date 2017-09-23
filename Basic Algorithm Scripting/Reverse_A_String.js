4069cb58a4038c99b91cf5afd5739005e4afbb7f
Serg <serg-gavel@users.noreply.github.com>
2017-09-24 00:52:50 +0300

    Add files via upload

---
+++ Basic Algorithm Scripting/Reverse_A_String.js
@@ -0,0 +1,10 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+reverseString("hello");
+
+reverseString("hello"); //should return a string.
+reverseString("hello"); //should become "olleh".
+reverseString("Howdy"); //should become "ydwoH".
+reverseString("Greetings from Earth"); //should return "htraE morf sgniteerG"
